831e3811b7473483a8ea35440f5a4412c7c5406d
israel <israel@DESKTOP-H47GNL1>
2022-07-13 20:19:20 -0600

    Agregar archivos de proyecto.

---
+++ tarea/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace tarea
+{
+    public class StartUp
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseDefaultFiles(); //index.html, default.html, index.asp, index.cshtml
+            app.UseStaticFiles();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapGet("/holaMundo", () => "Hola Mundo!");
+            });
+
+        }
+
+    }
+}
